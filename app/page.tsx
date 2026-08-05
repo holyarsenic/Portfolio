33357aed0293da8db500485556a5a886a0bf2374
Rohan Kamat <rohankamat7986@gmail.com>
2026-08-05 12:29:52 +0530

    feat: update components configuration and enhance intro component with profile image
    - Added "menuColor" and "menuAccent" properties to components.json.
    - Introduced a new registry for "@magicui" in components.json.
    - Enhanced the Intro component to include a profile image and updated text content for better presentation.
    - Added a new profile image file to the public directory.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center mt-10 bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center py-20 gap-4 h-full w-1/2">
       <Intro />
     </div>
   );
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,9 +1,25 @@
-
+import Image from "next/image";
+import Profile from "@/public/profile.jpg";
 
 const Intro = () => {
   return (
     <div className="flex items-center justify-center gap-4 w-full h-1/2">
-      <h1 className="text-4xl font-bold text-white">{`Hi, I'm Rohan`}</h1>
+      <Image 
+        src={Profile} 
+        alt="Profile" 
+        className="rounded-full w-40 h-40 object-cover" 
+      />
+
+      <div className="flex flex-col items-start">
+        <h1 className="text-4xl text-white font-heading">Rohan Kamat</h1>
+        <div className="relative h-24 w-full">
+        </div>
+        <p className="text-gray-400 text-sm mt-2">
+          I build modern, interactive web applications using TypeScript, 
+          React, 
+          Next.js, Node.js, and MongoDB, with a strong emphasis on clean UI, Performance, and User Experience
+        </p>
+      </div>
     </div>
   )
 }
