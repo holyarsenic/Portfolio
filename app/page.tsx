6eff5fa70a12ddcdb1dc1890c4aab9a913395fa2
Rohan Kamat <rohankamat7986@gmail.com>
2026-08-05 11:04:17 +0530

    feat: add new components and utilities for UI enhancement
    - Updated package.json to include new dependencies for UI components.
    - Created components.json for component configuration.
    - Added Navbar component for navigation.
    - Implemented Intro component for introductory display.
    - Developed Button component with variants and styles.
    - Introduced utility function for class name merging.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Intro from "@/components/intro";
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hello Guys!!
+    <div className="flex flex-col flex-1 items-center mt-10 bg-zinc-50 font-sans dark:bg-black">
+      <Intro />
     </div>
   );
 }
---
+++ components/intro.tsx
@@ -0,0 +1,11 @@
+
+
+const Intro = () => {
+  return (
+    <div className="flex items-center justify-center gap-4 w-full h-1/2">
+      <h1 className="text-4xl font-bold text-white">{`Hi, I'm Rohan`}</h1>
+    </div>
+  )
+}
+
+export default Intro
