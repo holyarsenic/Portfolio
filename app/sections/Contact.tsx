4c29cdfad9d08662e33e2bcfdb94d0b7f0aa6221
Rohan Kamat <rohankamat7986@gmail.com>
2026-08-07 19:07:17 +0530

    feat: enhance Contact section with styling and heading

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -2,8 +2,8 @@
 
 const Contact = () => {
   return (
-    <section>
-      
+    <section className="relative w-full overflow-hidden rounded-3xl px-10">
+      <h2 className="text-2xl">Contacts</h2>
     </section>
   )
 }
